977c3adf414458e5b52230eb76ce7162342d9667
Andrii Stepanets <ovrass@gmail.com>
2020-06-12 21:03:09 +0300

    added state to app and added ability to remove item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -55,7 +55,7 @@ export default class TodoListItem extends Component {
 
     render(){
 
-        const { label } = this.props;
+        const { label, onDeleted } = this.props;
 
         const { done, important } = this.state;
 
@@ -84,7 +84,8 @@ export default class TodoListItem extends Component {
                 </button>
 
                 <button type="button"
-                    className="btn btn-outline-danger btn-sm float-right">
+                    className="btn btn-outline-danger btn-sm float-right"
+                    onClick={ onDeleted }>
                     <i className="fa fa-trash-o" />
                 </button>
             </span>
